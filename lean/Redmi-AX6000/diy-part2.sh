ec54c857f3026343a5d42ef61db36d7a84af342a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-06-11 19:22:30 +0200

    Update diy-part2.sh

--- lean/Redmi-AX6000/diy-part2.sh
+++ lean/Redmi-AX6000/diy-part2.sh
@@ -12,12 +12,20 @@
 
 # Custom for REDMI AX6000 rmdir[ctrl_interface=/var/run/hostapd]: Permission denied
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#======================
 sed -i 's/zh_cn/auto/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
+#======================
 rm -rf feeds/packages/net/ddns-scripts/files/ddns.config
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/ddns.config feeds/packages/net/ddns-scripts/files/
+#======================
 rm -rf package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/lib/netifd/wireless/mac80211.sh package/kernel/mac80211/files/lib/netifd/wireless/
+#======================
 rm -rf package/kernel/mac80211/files/lib/wifi/mac80211.sh
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/lib/wifi/mac80211.sh package/kernel/mac80211/files/lib/wifi/
+#======================
 rm -rf feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
 cp $GITHUB_WORKSPACE/lean/Redmi-AX6000/data/turboacc feeds/luci/applications/luci-app-turboacc/root/etc/init.d/
+#======================
+rm -rf package/base-files/files/etc/banner
+cp $GITHUB_WORKSPACE/data/etc/banner package/base-files/files/etc/
